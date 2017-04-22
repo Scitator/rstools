91d72b37c0154b916e1c3e3c5958c58594c9817d
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2017-04-22 16:20:06 +0300

    package step 1

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rstools',
+    version='0.1',
+    description='Research tools.',
+    url='https://github.com/Scitator/rstools',
+    author='Kolesnikov Sergey',
+    author_email='scitator@gmail.com',
+    license='GPL3',
+    packages=['rstools'],
+    install_requires=[
+        'tensorflow==1.0.0',
+    ],
+    zip_safe=False)
