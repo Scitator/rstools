a7b36f9814f4d445db030705448e542c4f3224f4
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2017-04-28 13:25:03 +0300

    more pickle correctness

--- rstools/utils/os_utils.py
+++ rstools/utils/os_utils.py
@@ -8,16 +8,26 @@ def create_if_need(path):
         os.makedirs(path)
 
 
+def unpickle_data(path):
+    with open(path, "rb") as fout:
+        data = pickle.load(fout)
+        return data
+
+
+def pickle_data(data, path):
+    with open(path, "wb") as fout:
+        pickle.dump(data, fout)
+
+
+def masked_files(mask):
+    return sorted(glob(mask))
+
+
 def save_history(history, out_dir):
-    with open(os.path.join(out_dir, "history.pkl"), "wb") as fout:
-        pickle.dump(history, fout)
+    pickle_data(history, os.path.join(out_dir, "history.pkl"))
 
 
 def save_model(sess, saver, save_dir, model_global_step=None):
     create_if_need(save_dir)
     save_path = os.path.join(save_dir, "model.cpkl")
     saver.save(sess, save_path, global_step=model_global_step)
-
-
-def masked_files(mask):
-    return sorted(glob(mask))
