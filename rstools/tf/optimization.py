5366788cf76a700e8d972e0a5083e3e51ea3c42b
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2017-05-30 22:33:40 +0300

    simplified optimization + kwargs exp

--- rstools/tf/optimization.py
+++ rstools/tf/optimization.py
@@ -1,18 +1,21 @@
 import tensorflow as tf
 
 
-def update_varlist(loss, optimizer, var_list, grad_clip=5.0, global_step=None):
+def update_varlist(loss, optimizer, var_list, grad_clip=1.0, global_step=None, global_clip=False):
     grads_and_vars = optimizer.compute_gradients(loss, var_list=var_list)
-    # grads_and_vars = [(tf.clip_by_value(grad, -grad_clip, grad_clip), var) for grad, var in gvs]
-    gradients, variables = zip(*grads_and_vars)
-    clipped_gradients, _ = tf.clip_by_global_norm(gradients, clip_norm=grad_clip)
-    grads_and_vars = list(zip(clipped_gradients, variables))
+    if not global_clip:
+        grads_and_vars = [(tf.clip_by_norm(grad, grad_clip), var) for grad, var in grads_and_vars]
+    else:
+        gradients, variables = zip(*grads_and_vars)
+        clipped_gradients, _ = tf.clip_by_global_norm(gradients, clip_norm=grad_clip)
+        grads_and_vars = list(zip(clipped_gradients, variables))
     update_step = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
     return update_step
 
 
 def build_scope_optimization(
-        var_list, optimization_params=None, loss=None, optimizer=None, lr=None, global_step=None):
+        var_list, optimization_params=None, loss=None, optimizer=None, lr=None, global_step=None,
+        **kwargs):
     assert loss is not None
     optimization_params = optimization_params or {}
 
@@ -35,17 +38,20 @@ def build_scope_optimization(
         loss, optimizer,
         var_list=var_list,
         grad_clip=grad_clip,
-        global_step=global_step)
+        global_step=global_step,
+        **kwargs)
     return optimizer, train_op
 
 
 def build_model_optimization(
         model, optimization_params=None,
-        loss=None, optimizer=None, lr=None):
+        loss=None, optimizer=None, lr=None,
+        **kwargs):
     model.loss = model.loss if model.loss is not None else loss
     model.optimizer, model.train_op = build_scope_optimization(
         var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
                                    scope=model.scope),
         optimization_params=optimization_params,
         loss=model.loss, optimizer=optimizer, lr=lr,
-        global_step=model.global_step)
+        global_step=model.global_step,
+        **kwargs)
