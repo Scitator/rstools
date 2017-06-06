a1663f55087767d176a2be2c63f45cd1dd46716f
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2017-06-06 15:01:05 +0300

    now with tensorboard logger

--- rstools/tf/training.py
+++ rstools/tf/training.py
@@ -4,11 +4,15 @@ import collections
 from tqdm import trange
 import os
 import tensorflow as tf
+from datetime import datetime
+from .logger import Logger
 from ..utils.os_utils import create_if_need, save_history, save_model
 from ..visualization.plotter import plot_all_metrics
 
 
-def run_generator(sess, run_keys, result_keys, feed_keys, data_gen, n_batch=-1):
+def run_generator(
+        i_step, sess, run_keys, result_keys, feed_keys, data_gen,
+        n_batch=-1, logger=None):
     history = collections.defaultdict(list)
 
     for i_batch, feed_values in enumerate(data_gen):
@@ -19,6 +23,10 @@ def run_generator(sess, run_keys, result_keys, feed_keys, data_gen, n_batch=-1):
         for i, key in enumerate(result_keys):
             history[key].append(run_result[i])
 
+        if logger is not None:
+            for key, value in history.items():
+                logger.scalar_summary(key, value, i_step + i_batch)
+
         if i_batch + 1 >= n_batch > 0:
             break
     return history
@@ -28,13 +36,14 @@ def run_train(sess, train_gen, train_params, val_gen=None, val_params=None, run_
     run_params = run_params or {}
 
     n_epochs = run_params.get("n_epochs", 100)
-    log_dir = run_params.get("log_dir", "./logs")
+    log_dir = run_params.get("log_dir", "./logs_{:%Y%m%d_%H%M%S}".format(datetime.now()))
     plotter_dir = run_params.get("plotter_dir", "plotter")
     model_dir = run_params.get("model_dir", "model")
     checkpoint_every = run_params.get("checkpoint_every", 10)
     model_global_step = run_params.get("model_global_step", None)
     create_if_need(log_dir)
 
+    logger = Logger(log_dir) if run_params.get("use_tensorboard", False) else None
     train_iter_epoch = train_params.get("n_batch", -1) < 0
     val_iter_epoch = val_params.get("n_batch", -1) < 0
 
@@ -45,14 +54,15 @@ def run_train(sess, train_gen, train_params, val_gen=None, val_params=None, run_
         n_epochs,
         desc="",
         leave=True)
+    i_step = 0
 
     for i_epoch in tr:
-        i_epoch += 1
         if train_iter_epoch:
             train_gen, train_gen_copy = itertools.tee(train_gen, 2)
         else:
             train_gen_copy = train_gen
-        train_epoch_history = run_generator(sess, data_gen=train_gen_copy, **train_params)
+        train_epoch_history = run_generator(
+            i_step, sess, data_gen=train_gen_copy, logger=logger, **train_params)
         for metric in train_epoch_history:
             history[metric].append(np.mean(train_epoch_history[metric]))
 
@@ -61,10 +71,13 @@ def run_train(sess, train_gen, train_params, val_gen=None, val_params=None, run_
                 val_gen, val_gen_copy = itertools.tee(val_gen, 2)
             else:
                 val_gen_copy = val_gen
-            val_epoch_history = run_generator(sess, data_gen=val_gen_copy, **val_params)
+            val_epoch_history = run_generator(
+                i_step, sess, data_gen=val_gen_copy, logger=logger, **val_params)
             for metric in val_epoch_history:
                 history[metric].append(np.mean(val_epoch_history[metric]))
 
+        i_step += [len(value) for key, value in train_epoch_history.items()][0]  # a bit of hack
+        i_epoch += 1
         if i_epoch % checkpoint_every == 0:
             checkpoint_dir = os.path.join(log_dir, model_dir, str(i_epoch))
             save_model(
